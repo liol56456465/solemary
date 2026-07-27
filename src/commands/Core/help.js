acb8bf8f5e44f6179e8b846d052adb9eb065589d
liol56456465 <vytispileckas14@gmail.com>
2026-07-27 05:09:03 +0300

    Remove bug report button functionality from help menu
    - Removed BUG_REPORT_BUTTON_ID constant
    - Removed bugReportButton code
    - Kept only support server button visible

--- src/commands/Core/help.js
+++ src/commands/Core/help.js
@@ -18,7 +18,6 @@ const __dirname = path.dirname(__filename);
 
 const CATEGORY_SELECT_ID = "help-category-select";
 const ALL_COMMANDS_ID = "help-all-commands";
-const BUG_REPORT_BUTTON_ID = "help-bug-report";
 const HELP_MENU_TIMEOUT_MS = 5 * 60 * 1000;
 
 const CATEGORY_ICONS = {
@@ -113,11 +112,6 @@ export async function createInitialHelpMenu(client) {
     });
     embed.setTimestamp();
 
-    const bugReportButton = new ButtonBuilder()
-        .setCustomId(BUG_REPORT_BUTTON_ID)
-        .setLabel("Report Bug")
-        .setStyle(ButtonStyle.Danger);
-
     const supportButton = new ButtonBuilder()
         .setLabel("Support Server")
         .setURL("https://discord.gg/8x8w6gbEuZ")
@@ -130,7 +124,6 @@ export async function createInitialHelpMenu(client) {
     );
 
     const buttonRow = new ActionRowBuilder().addComponents([
-        bugReportButton,
         supportButton,
     ]);
 
